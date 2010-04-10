43c8bb6a1f64dc6dab8baa37b3906c6a0646bb4a
Daniel Roseman <daniel@roseman.org.uk>
2010-04-10 22:43:01 +0100

    Added setup.py to make installable.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "django_efficient",
+    version = "0.1",
+    packages = [
+        "efficient",
+    ],
+    author = "Daniel Roseman",
+    author_email = "daniel@roseman.org.uk",
+    description = "Utilities to make Django ORM queries more efficiently.",
+    url = "http://github.com/danielroseman/django-efficient",
+)
